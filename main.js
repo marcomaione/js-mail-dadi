a72cd94f2e8d21ba1d47e3e028b9c5e264f2c48c
marco maione <maionemarco14@gmail.com>
2022-01-25 17:19:20 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -3,14 +3,25 @@
 const myarray = ['marco@gmail.it', 'matteo@gmail.it', 'sara@gmail.it'];
 
 const mail = prompt('inserisci la tua mail');
-
+ 
 const lunghezzaArray = myarray.length;
 console.log(lunghezzaArray)
 
-for ( let i=0; i<myarray.length; i++ ) {
-    if (myarray[i] == mail) {
-        alert("email inserita è corretta");
-    } else {
-        alert("email inserita non è corretta");
+for (let i=0; i<myarray.length; i++) {
+
+    if (myarray[i] == (mail)) {
+        let login = true
+    }else {
+        let login = false
     }
 }
+
+if (login == true) {
+    alert("email inserita è corretta");
+}else {
+    alert("email inserita non è corretta");
+}
+
+
+
+
